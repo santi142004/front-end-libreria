c3a2abc684b6777d5695af058c3cd9d3fc7482db
santi142004 <santiagobarreralopez2004@gmail.com>
2023-10-22 20:24:00 -0500

    logo y funcionalidades

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import './Header.css'
+import img from '../../../public/logo.png'
 export const Header = () => {
   return (
     <header>
+      <img className='logo' src={img} alt='logo' />
     <h1>Harry Books -Tienda Online</h1>
     </header>
   )
